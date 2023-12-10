77f664a67fb3cd40e51d7a959fbb6cac1b683386
dantecatalfamo <dante.catalfamo@gmail.com>
2023-12-10 02:15:44 -0500

    Test comparison table

--- server_test.go
+++ server_test.go
@@ -66,11 +66,11 @@ func TestServer(t *testing.T) {
 		}
 		_, err = s.db.Exec("INSERT INTO media(path, sha1sum, score, matches) VALUES ('a', 'abc', 123, 0), ('b', 'bcd', 456, 12)")
 		if err != nil {
-			t.Errorf("failed to insert test data: %s", err)
+			t.Fatalf("failed to insert test data: %s", err)
 		}
 		media1, err := s.GetMediaInfo(1)
 		if err != nil {
-			t.Errorf("failed to get media: %s", err)
+			t.Fatalf("failed to get media: %s", err)
 		}
 		if media1.Id != 1 {
 			t.Errorf("expected media1.Id to be 1, found %d", media1.Id)
@@ -115,11 +115,11 @@ func TestServer(t *testing.T) {
 		}
 		row1, err := s.InsertMedia("fakepath", "aaa")
 		if err != nil {
-			t.Error(err)
+			t.Fatalf("failed to insert media: %s", err)
 		}
 		row2, err := s.InsertMedia("anotherfakepath", "bbb")
 		if err != nil {
-			t.Error(err)
+			t.Fatalf("failed to insert media: %s", err)
 		}
 		if row1 == row2 {
 			t.Errorf("expected to separate rows, returned %d and %d", row1, row2)
@@ -132,14 +132,14 @@ func TestServer(t *testing.T) {
 		}
 		rowCount, err := s.MediaCount()
 		if err != nil {
-			t.Errorf("failed to get media count: %s", err)
+			t.Fatalf("failed to get media count: %s", err)
 		}
 		if rowCount != 2 {
 			t.Errorf("expected 2 records, found %d", rowCount)
 		}
 		media1, err := s.GetMediaInfo(row1)
 		if err != nil {
-			t.Errorf("failed to get media into: %s", err)
+			t.Fatalf("failed to get media into: %s", err)
 		}
 		if media1.Path != "fakepath" {
 			t.Errorf("expected media1.Path to be \"fakepath\", found %s", media1.Path)
@@ -165,18 +165,18 @@ func TestServer(t *testing.T) {
 		}
 		row1, err := s.InsertMedia("fakepath", "aaa")
 		if err != nil {
-			t.Errorf("failed to insert media: %s", err)
+			t.Fatalf("failed to insert media: %s", err)
 		}
 		row2, err := s.InsertMedia("differentpath", "aaa")
 		if err != nil {
-			t.Errorf("failed to update media path: %s", err)
+			t.Fatalf("failed to update media path: %s", err)
 		}
 		if row1 != row2 {
 			t.Errorf("expected row1 == row2, instead found %d and %d", row1, row2)
 		}
 		media, err := s.GetMediaInfo(row1)
 		if err != nil {
-			t.Errorf("failed to get media info: %s", err)
+			t.Fatalf("failed to get media info: %s", err)
 		}
 		if media.Path != "differentpath" {
 			t.Errorf("expectd media.Path to be \"differentpath\", found: %s", media.Path)
@@ -203,30 +203,32 @@ func TestServer(t *testing.T) {
 			}
 			winnerId, err := s.InsertMedia("fakepath", "aaa")
 			if err != nil {
-				t.Errorf("failed to insert media: %s", err)
+				t.Fatalf("failed to insert media: %s", err)
 			}
 			_, err = s.db.Exec("UPDATE media SET score = ? WHERE id = ?", test.winnerBefore, winnerId)
 			if err != nil {
-				t.Errorf("failed to set winner score: %s", err)
+				t.Fatalf("failed to set winner score: %s", err)
 			}
 			loserId, err := s.InsertMedia("alsofakepath", "bbb")
 			if err != nil {
-				t.Errorf("failed to insert media: %s", err)
+				t.Fatalf("failed to insert media: %s", err)
 			}
+
 			_, err = s.db.Exec("UPDATE media SET score = ? WHERE id = ?", test.loserBefore, loserId)
 			if err != nil {
-				t.Errorf("failed to set loser score: %s", err)
+				t.Fatalf("failed to set loser score: %s", err)
 			}
 			if err := s.UpdateScores(winnerId, loserId); err != nil {
-				t.Errorf("failed to update scores: %s", err)
+				t.Fatalf("failed to update scores: %s", err)
 			}
+
 			winnerInfo, err := s.GetMediaInfo(winnerId)
 			if err != nil {
-				t.Errorf("failed to get winner media info: %s", err)
+				t.Fatalf("failed to get winner media info: %s", err)
 			}
 			loserInfo, err := s.GetMediaInfo(loserId)
 			if err != nil {
-				t.Errorf("failed to get loser media info: %s", err)
+				t.Fatalf("failed to get loser media info: %s", err)
 			}
 			if winnerInfo.Score != test.winnerAfter {
 				t.Errorf("expected winnerInfo.Score to be %d, found %d", test.winnerAfter, winnerInfo.Score)
@@ -239,6 +241,25 @@ func TestServer(t *testing.T) {
 			}
 			if loserInfo.Matches != 1 {
 				t.Errorf("expected loserInfo.Matches to be 1, found %d", loserInfo.Matches)
+			}
+
+			row := s.db.QueryRow("SELECT winner_id, loser_id, points FROM comparisons")
+			if row.Err() != nil {
+				t.Fatalf("failed to select from comparison table: %s", err)
+			}
+			var compWinnerId, compLoserId int64
+			var compPoints int
+			if err := row.Scan(&compWinnerId, &compLoserId, &compPoints); err != nil {
+				t.Fatalf("failed to scan comparisons row: %s", err)
+			}
+			if compWinnerId != winnerId {
+				t.Errorf("expected compWinnerId to be %d, found %d", winnerId, compWinnerId)
+			}
+			if compLoserId != loserId {
+				t.Errorf("expected compLoserId to be %d, found %d", loserId, compLoserId)
+			}
+			if compPoints != test.winnerAfter - test.winnerBefore {
+				t.Errorf("expected compPoints to be %d, found %d", test.winnerAfter - test.winnerBefore, compPoints)
 			}
 		}
 	})
